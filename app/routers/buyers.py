3e0a303b0fc540be8b6c33ddbebb275b4fe2e4f5
revanth <revanthraigondhi@gmail.com>
2026-02-27 20:30:34 +0530

    clean code in buyer,sellers.py

--- app/routers/buyers.py
+++ app/routers/buyers.py
@@ -2,17 +2,25 @@ from fastapi import APIRouter,Depends,HTTPException,status
 import app.models as models
 from app.database import get_db
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 import app.schemas as schemas
-from typing import Optional,List
+from typing import Annotated,List
 
 router = APIRouter(
-    prefix='/buyers'
+    prefix='/buyers',
+    tags = ['buyers']
 )
 
-@router.get('/browse_products',response_model=List[schemas.BrowseProducts])
-def browse_products(db: Session = Depends(get_db),limits:int = 15,search:Optional[str] =''):
-    products = db.query(models.Products).filter(models.Products.name.icontains(search)).limit(limits).all()
+db_dep = Annotated[Session, Depends(get_db)]
 
+@router.get('/browse_products',response_model=List[schemas.BrowseProducts])
+def browse_products(db: db_dep,limits:int = 15,search:str|None = None):
+    try:
+        products = db.query(models.Products).filter(models.Products.name.icontains(search)).limit(limits).all()
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database error")
+    
     if not products:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail= f'cannot find {search}')
--- app/routers/sellers.py
+++ app/routers/sellers.py
@@ -2,18 +2,32 @@ from fastapi import APIRouter,Depends,HTTPException,status
 import app.models as models
 from app.database import get_db
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 import app.schemas as schemas
 from app.oauth2 import get_current_user
-from typing import Optional 
+from typing import Annotated 
 
 router = APIRouter(
-    prefix='/sellers'
+    prefix='/sellers',
+    tags= ['sellers']
 )
 
-@router.post('/create_product')
-def create_product(product:schemas.ProductCreate,db: Session = Depends(get_db),current_user : int = Depends(get_current_user)):
+
+db_dep = Annotated[Session, Depends(get_db)]
+user_dep = Annotated[models.Users, Depends(get_current_user)]
+
+
+class Context:
+    def __init__(self, db: db_dep, user: user_dep):
+        self.db = db
+        self.user = user
+MarketContext = Annotated[Context, Depends()]
+
+
+@router.post('/create_product',status_code=status.HTTP_201_CREATED)
+def create_product(product:schemas.ProductCreate,ctx: MarketContext):
     
-    if current_user.role != 'seller':
+    if ctx.user.role != 'seller':
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, 
             detail="Only sellers can create products"
@@ -23,35 +37,49 @@ def create_product(product:schemas.ProductCreate,db: Session = Depends(get_db),c
         name = product.name,
         price = product.price,
         stock = product.quantity,
-        owner_id=current_user.id   
+        owner_id=ctx.user.id   
     )
+    try:
+        ctx.db.add(new_product)
+        ctx.db.commit()
+        ctx.db.refresh(new_product)
+    except:
+        ctx.db.rollback()
+        raise HTTPException(status_code=500, detail="Database error")
 
-    db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
 
     return new_product
 
-@router.put('/update_product')
-def update_product(product_name:str,product_update:schemas.ProductUpdate,db: Session = Depends(get_db),current_user : int = Depends(get_current_user),search:Optional[str] = ''):
-     
-    product_query = db.query(models.Products).filter(
-        models.Products.name == product_name,
-        models.Products.owner_id == current_user.id
-    )
-
-    existing_product = product_query.first()
-
-    if not existing_product:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail= f'there is no product named - {product_name}')
+@router.put('/update_product/{product_id}')
+def update_product(product_id:int,product_update:schemas.ProductUpdate,ctx: MarketContext,search: str | None = ''):
     
-    product_query.update(product_update.model_dump(exclude_unset=True))
-    db.commit()
+    if ctx.user.role != "seller":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only sellers can update products"
+        ) 
+    
 
-    updated_product = product_query.first()
+    product = ctx.db.query(models.Products).filter(
+        models.Products.id == product_id,
+        models.Products.owner_id == ctx.user.id
+    ).first()
 
-    return {'message':'product updated',
-            'product' : updated_product}
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found or or unauthorized")
+    
+    update_data = product_update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(product, key, value)
+
+    
+    try:
+        ctx.db.commit()
+        ctx.db.refresh(product)
+    except SQLAlchemyError:
+            ctx.db.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database error")
+    
+    return {"status": "success", "message": f"{product_id} updated"}
     
      
