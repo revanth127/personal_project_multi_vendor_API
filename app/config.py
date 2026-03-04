07065b61f8fc1169508f5b8be7eb992261bb6c3c
revanth <revanthraigondhi@gmail.com>
2026-03-04 19:09:27 +0530

    default env variables in config

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     
-    DATABASE_URL: str
-    DATABASE_URL_TEST : str
-    SECRET_KEY : str
+    DATABASE_URL: str = "sqlite:///./test.db"
+    DATABASE_URL_TEST: str = "sqlite:///./test.db"
+    SECRET_KEY: str = "dev-secret"
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
