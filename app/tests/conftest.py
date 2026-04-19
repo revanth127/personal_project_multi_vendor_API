d1c9f36ebe5a56cec0bd97a130c9239338ff10a2
revanth <revanthraigondhi@gmail.com>
2026-04-19 21:11:51 +0530

    fixed bug in tests

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,15 +1,24 @@
 import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
 
 @pytest.fixture
-def test_user_seller(client):
+def client():
+    return TestClient(app)
+
+
+@pytest.fixture
+def test_user_seller():
     return {
         "email": "tester_new@gmail.com",
         "password": "securepassword123",
         "role": "seller"
     }
 
+
 @pytest.fixture
-def test_user_buyer(client):
+def test_user_buyer():
     return {
         "email": "tester2_new@gmail.com",
         "password": "securepassword1234",
